21a564fbae345faead124ee3dd4cf275b062b228
unknown <caballeroian97@gmail.com>
2018-05-16 19:11:13 -0300

    Le di onda al agregar viaje pero falta arreglarle un par de cosas

--- publicarviaje.php
+++ publicarviaje.php
@@ -15,12 +15,13 @@
 <body class="FondoInicio">
 <?php
 include "Header.php";
+include "MenuBarra.php";
     if (isset($usuarioID)){
     ?>
-        <div class= "publicarUnviaje">
+        <div class= "registrar">
         <form method="POST" action="CrearViaje.php" class="input" onsubmit="return ValidarViaje()">
           <label class="LabelFormularios"> Origen </label>
-          <select name = "origen" value="<?php echo $origen ?>"> 
+          <select class="FormularioVehiculos" name = "origen" value="<?php echo $origen ?>"> 
                 <?php
                   $consulta_ciudades = "SELECT * FROM ciudades ORDER BY ciudad ASC"; 
                   $result_origen = mysqli_query($link,$consulta_ciudades); ?>
@@ -34,7 +35,7 @@ include "Header.php";
           </select>
           <br></br>
           <label class="LabelFormularios"> Destino </label>
-          <select name = "destino" value="<?php echo $destino ?>"> 
+          <select class="FormularioVehiculos" name = "destino" value="<?php echo $destino ?>"> 
                 <?php
                   $consulta_ciudades = "SELECT * FROM ciudades ORDER BY ciudad ASC";
                   $result_Destino = mysqli_query($link,$consulta_ciudades); ?>
@@ -47,21 +48,21 @@ include "Header.php";
           </select>
           <br></br>
           <label class="LabelFormularios"> Dia </label>
-          <input type="text" id="fecha" name="fecha" class="FormularioCrearviaje" placeholder="Ingrese fecha AAAA-MM-DD..">
+          <input type="text" id="fecha" name="fecha" class="FormularioVehiculos" placeholder="Ingrese fecha AAAA-MM-DD..">
           <br></br>
           <label class="LabelFormularios"> Hora de partida </label>
-          <input type="int" id="horapartida" name="horapartida" class="FormularioCrearviaje">
-          <input type="int" id="minutospartida" name="minutospartida" class="FormularioCrearviaje">
+          <input type="int" id="horapartida" name="horapartida" class="FormularioVehiculos">
+          <input type="int" id="minutospartida" name="minutospartida" class="FormularioVehiculos">
           <br></br>
           <label class="LabelFormularios"> Duracion estimada </label>
-          <input type="int" id="duracionhoras" name="duracionhoras" class="FormularioCrearviaje">
-          <input type="int" id="duracionmin" name="duracionmin" class="FormularioCrearviaje">
+          <input type="int" id="duracionhoras" name="duracionhoras" class="FormularioVehiculos">
+          <input type="int" id="duracionmin" name="duracionmin" class="FormularioVehiculos">
           <br></br>
           <label class="LabelFormularios"> Informacion adicional </label>
-          <textarea name = "texto" size=200></textarea>
+          <textarea class="FormularioVehiculos" name = "texto" size=200></textarea>
           <br></br>
           <label class="LabelFormularios"> Vehiculo </label>
-          <select name = "vehiculo" value="<?php echo $vehiculo ?>"> 
+          <select class="FormularioVehiculos" name = "vehiculo" value="<?php echo $vehiculo ?>"> 
                 <?php
                   $consulta_vehiculos = "SELECT * FROM vehiculos WHERE idUsuario=$ID"; 
                   $result_vehiculos = mysqli_query($link,$consulta_vehiculos); ?>
@@ -73,9 +74,9 @@ include "Header.php";
                 ?>
           </select>
           <label class="LabelFormularios"> Precio </label>
-          <input type="number" id="precio" name="precio" class="FormularioCrearviaje" placeholder="Ingrese precio por asiento...">
+          <input type="number" id="precio" name="precio" class="FormularioVehiculos" placeholder="Ingrese precio por asiento...">
           <br></br>
-          <div><input type="submit" class="BotonCrearViaje" value="Agregar"></div>
+          <div><input type="submit" class="BotonRegistrar" value="Agregar"></div>
 
         </form>
         </div>

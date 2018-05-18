2dcbf6b41e015e3447ac90b32c0666679b462a35
unknown <caballeroian97@gmail.com>
2018-05-18 12:40:22 -0300

    Caja de busqueda compartamiento inapropiado dependiendo del zoom,fixed

--- MenuBarra.php
+++ MenuBarra.php
@@ -1,14 +1,15 @@
 <div class="ArreglarFondoMenuBarra">
-	<div class="menuBarra" style="width:100%">
+	<div class="menuBarra">
 		<ul>
 			<li><a href="Inicio.php">Inicio</a></li>
 			<li><a href="PublicarViaje.php">Publicar</a></li>
 			<li><a href="#">Vehiculos</a>
 				<ul>
-				<li><a href="#">Agregar</a></li>
-				<li><a href="#">Modificar</a></li>
-				<li><a href="#">Borrar</a></li>
-			</ul>
+					<li><a href="#">Mis vehiculos</a></li>
+					<li><a href="#">Agregar</a></li>
+					<li><a href="#">Modificar</a></li>
+					<li><a href="#">Borrar</a></li>
+				</ul>
 			</li>
 			<li><a href="#">Perfil</a></li>
 			<li><a href="#">Editar Perfil</a></li>
